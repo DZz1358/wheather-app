de3b6e0d638f7601f0836d015f925701f756669e
Den <saloed2206@gmail.com>
2024-08-15 16:09:44 +0300

    added input and get geolocation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 
@@ -9,6 +9,41 @@ import { HeaderComponent } from "./components/header/header.component";
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'wheather-app';
+export class AppComponent implements OnInit {
+  title = 'weather-app';
+  weatherData: any;
+  error: string = '';
+
+  ngOnInit(): void {
+    this.getLocation();
+  }
+
+  getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          const lat = position.coords.latitude;
+          const lon = position.coords.longitude;
+          this.getWeather(lat, lon);
+        },
+        error => {
+          this.error = 'Невозможно определить местоположение';
+        }
+      );
+    } else {
+      this.error = 'Геолокация не поддерживается вашим браузером';
+    }
+  }
+
+
+
+  getWeather(lat: number, lon: number) {
+    // this.weatherService.getWeather(lat, lon).subscribe(
+    //   data => this.weatherData = data,
+    //   error => this.error = 'Не удалось получить данные о погоде'
+    // );
+
+    console.log('lat', lat, 'lon', lon)
+
+  }
 }
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -6,8 +6,17 @@ import { Injectable } from '@angular/core';
 })
 export class WeatherService {
 
+  API_URL = 'https://api.openweathermap.org'
   API_KEY = '62ab7928b8dfae9b7a12dbad00398c62'
 
   constructor(private http: HttpClient) { }
 
+  getCityWeather(city: string) {
+    return this.http.get(`XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX${city}&appid=${this.API_KEY}&units=metric`)
+  }
+
+
+  getCurrentWeather(city: string) {
+    return this.http.get(`${this.API_URL}/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid=${this.API_KEY}`)
+  }
 }
